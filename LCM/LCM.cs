42e24510ab8164ab20afd1144182a6d8fb555ba6
mr-fool <mr-fool@users.noreply.github.com>
2015-07-04 23:21:41 -0600

    Update LCM.cs

--- LCM/LCM.cs
+++ LCM/LCM.cs
@@ -10,12 +10,36 @@ public class LCM
             Environment.Exit(1);
         }
         int[] numbers = new int[args.Length];
+        //convert args to int
         for (int i = 0; i < args.Length; i++)
         {
-            Console.WriteLine(args[i]);
             numbers[i] = int.Parse(args[i]);
             Console.WriteLine(numbers[i]);
         }
+        int result = 1;
+        for (int i = 0; i < args.Length; i++)
+        {
+            result = lcm(result, numbers[i]);
+        }
+        Console.WriteLine("LCM of given numbers = {0}", result);
     }
-
+    public static int lcm(int a, int b)
+    {
+        int gcd = gcdNumbers(a, b);
+        return (a * b) / gcd;
+    }
+    public static int gcdNumbers(int a, int b)
+    {
+        int temp;
+        if (a > b)
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b % a == 0)
+            return a;
+        else
+            return gcdNumbers(b % a, a);
+    }
 }
