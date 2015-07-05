00635218ca9a52b2244660c1d5804d0b2d689b8e
mr-fool <mr-fool@users.noreply.github.com>
2015-07-04 22:12:55 -0600

    Update LCM.cs

--- LCM/LCM.cs
+++ LCM/LCM.cs
@@ -6,7 +6,7 @@ public class LCM
         Console.WriteLine("Please enter a list of numbers that you wish to find lcm\n");
         Console.WriteLine("You have entered {0} number of numbers\n", args.Length);
         int[] numbers = new int[args.Length];
-        for (int i = 1; i < args.Length; i++)
+        for (int i = 0; i < args.Length; i++)
         {
             numbers[i] = Int.Parse(args[0]);
         }
