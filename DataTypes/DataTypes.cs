948a996e6668f83ddbc527955cc29806c305d174
mr-fool <mr-fool@users.noreply.github.com>
2015-08-25 18:28:42 -0600

    Create DataTypes.cs

---
+++ DataTypes/DataTypes.cs
@@ -0,0 +1,27 @@
+using System;
+
+
+namespace DataTypesCollection
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of bool: {0}", sizeof(bool));
+            Console.WriteLine("Size of byte: {0}", sizeof(byte));
+            Console.WriteLine("Size of char: {0}", sizeof(char));
+            Console.WriteLine("Size of decimal: {0}", sizeof(decimal));
+            Console.WriteLine("Size of double: {0}", sizeof(double));
+            Console.WriteLine("Size of float: {0}", sizeof(float));
+            Console.WriteLine("Size of Int32: {0}", sizeof(Int32));
+            Console.WriteLine("Size of Int64: {0}", sizeof(Int64));
+            Console.WriteLine("Size of long: {0}", sizeof(long));
+            Console.WriteLine("Size of sbyte: {0}", sizeof(sbyte));
+            Console.WriteLine("Size of short: {0}", sizeof(short));
+            Console.WriteLine("Size of unit: {0}", sizeof(uint));
+            Console.WriteLine("Size of ulong: {0}", sizeof(ulong));
+            Console.WriteLine("Size of ushort: {0}", sizeof(ushort));
+            Console.ReadLine();
+        }
+    }
+}
